72001744a82e8acaa7fac194151036218cf94e4b
hystrangea <avalapena@gmail.com>
2023-08-31 14:55:36 -0700

    added transition to button rotation

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// Variable to hold the current angle of rotation
+  let rotation = 0;
+
+// How much to rotate the image at a time
+ const angle = 45;
+
+
 function bangClicked(){
+    //this part turns the button blue when clicked on
     console.log("bang clicked")
     var buttonElement =document.getElementById("bangButton")
-    buttonElement.style.backgroundColor="red"
+    buttonElement.style.backgroundColor="blue"
+
+        
+
+
+   //this part sets the rotation
+    rotation = (rotation + angle) % 360;
+    buttonElement.style.transform=`rotate(${rotation}deg)`;
+
+
+    
+
 }
